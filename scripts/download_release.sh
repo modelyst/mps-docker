fb66998d87af5a41fd49764b15c4b24cc70e30dc
Michael Statt <michael.statt@modelyst.io>
2022-12-15 11:59:13 -0500

    clean up and simplify docker deployments and add download script

--- scripts/download_release.sh
+++ scripts/download_release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh 
 
+echo "Downloading Postgres Release from Caltech data..."
 DOWNLOAD_LINK=https://data.caltech.edu/records/4kk39-69x76/files/public_release_20221115.tar.gz?download=1
 wget  $DOWNLOAD_LINK \
  -O ./data/releases/postgres/public_release.tar.gz
+
+echo "Downloading Neo4j release from S3..."
+PROFILE=${PROFILE:-default}
+aws s3 --profile $PROFILE cp s3://modelyst.rw/neo4j_dumps/public-release-neo4j-Dec-1-2022-09-01-23.dump ./data/releases/public-release.dump
--- scripts/loader-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-neo4j-admin \
-    load \
-    --database=neo4j \
-    --from=/releases/${BACKUP_FILE:-public-release.dump} \
-    --force
