1c203455c0d0dfebdcb70acc6dadc2ff0d1ba09f
Michael Statt <michael.statt@modelyst.io>
2023-03-30 13:33:38 -0400

    chore: update the download release links

--- scripts/download_release.sh
+++ scripts/download_release.sh
@@ -1,10 +1,12 @@
 #!/bin/sh 
 
 echo "Downloading Postgres Release from Caltech data..."
-DOWNLOAD_LINK=https://data.caltech.edu/records/4kk39-69x76/files/public_release_20221115.tar.gz?download=1
+DOWNLOAD_LINK=https://data.caltech.edu/records/hjfx4-a8r81/files/public_release_20230228.tar.gz?download=1
 wget  $DOWNLOAD_LINK \
  -O ./data/releases/postgres/public_release.tar.gz
 
 echo "Downloading Neo4j release from S3..."
 PROFILE=${PROFILE:-default}
-aws s3 --profile $PROFILE cp s3://modelyst.rw/neo4j_dumps/public-release-neo4j-Dec-1-2022-09-01-23.dump ./data/releases/public-release.dump
+DOWNLOAD_LINK=https://data.caltech.edu/records/h88fq-dk449/files/public-release-neo4j-20230228.dump?download=1
+wget  $DOWNLOAD_LINK \
+ -O ./data/releases/neo4j/neo4j.dump
